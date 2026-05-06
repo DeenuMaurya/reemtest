774df79150d21d8dcd5ea5c07c293452ea51ba9e
Chandan Maurya <cmaurya46@gmail.com>
2026-05-06 22:02:01 +0530

    rail

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from rag import ask_patient
 
 app = FastAPI()
 
+@app.get("/")
+def root():
+    return {"status": "ok"}
 
 class Query(BaseModel):
-
     question: str
-
 
 @app.post("/chat")
 def chat(data: Query):
-
-    answer = ask_patient(
-        data.question
-    )
-
-    return {
-        "answer": answer
-    }
+    answer = ask_patient(data.question)
+    return {"answer": answer}
